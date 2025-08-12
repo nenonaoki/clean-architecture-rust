4e1aba56f99b20b396c3c7c5c8f4af2f10d1c84e
Naoki Okayama <naoki.okayama@neno.co.jp>
2025-08-12 22:53:06 +0900

    Move container

--- corporation-api/src/infrastructure/mod.rs
+++ corporation-api/src/infrastructure/mod.rs
@@ -1,56 +1,9 @@
-use std::any::Any;
-use std::collections::HashMap;
 use std::sync::Arc;
-use std::sync::RwLock;
 
 pub mod database;
 pub mod persistences;
 
-use persistences::{project::ProjectRepositoryImpl, user::UserRepositoryImpl};
-
-#[derive(Debug, thiserror::Error)]
-pub enum DIError {
-    #[error("dependency not found")]
-    NotFound,
-
-    #[error("unknown error")]
-    Unknown,
-}
-
-pub struct Container {
-    dependencies: RwLock<HashMap<String, Arc<dyn Any + Send + Sync>>>,
-}
-
-impl Container {
-    pub fn new() -> Self {
-        Self {
-            dependencies: RwLock::new(HashMap::new()),
-        }
-    }
-
-    pub fn register<T>(&self, name: &str, dependency: T) -> Result<(), DIError>
-    where
-        T: 'static + Send + Sync,
-    {
-        let mut dependencies = self.dependencies.write().unwrap();
-
-        dependencies.insert(name.to_string(), Arc::new(dependency));
-        Ok(())
-    }
-
-    pub fn resolve<T>(&self, name: &str) -> Result<Arc<T>, DIError>
-    where
-        T: 'static + Send + Sync,
-    {
-        let dependencies = self.dependencies.read().unwrap();
-        let dependency = dependencies.get(name).ok_or_else(|| DIError::NotFound)?;
-
-        dependency
-            .clone()
-            .downcast::<T>()
-            .map_err(|_| DIError::Unknown)
-    }
-}
+use persistences::{Container, project::ProjectRepositoryImpl, user::UserRepositoryImpl};
 
 pub struct AppData {
     pub container: Container,
